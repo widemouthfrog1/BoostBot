696ac21c9917b56b8cf1230c35574d630af0fbc9
Karl Bennett <widemouthfrog1@gmail.com>
2021-02-15 13:05:39 +1300

    Add simple vector class

---
+++ CSharpBot/Bot/Vector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    class Vector
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
